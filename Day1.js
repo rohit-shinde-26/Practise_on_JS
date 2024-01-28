1d3eac89c54f4a433f7c08a52cde83fe7a4035f9
rohit-shinde-26 <100394511+rohit-shinde-26@users.noreply.github.com>
2024-01-28 06:38:06 +0000

    here the datatypes are in day1

--- Day1.js
+++ Day1.js
@@ -1,4 +1,37 @@
 // Start from topic 1
+
+// variables are container for storing values
 var a = 17;
-console.log(a)
-console.log(typeof a)
+// console.log(a);
+// console.log(typeof a);
+
+var num = 1;
+var name = "Ram";
+var email = "rohitshinde26072003@gmail.com";
+
+console.log(typeof num); // shows the typeof an variable
+
+console.table([num, name, email]); // it makes table is in console
+
+// var is global variable thats why we can change it anywhere it is declare,update and redeclare
+
+console.log(
+  "----------------------- DATATYPES----------------------------------------",
+);
+
+// 1. Primitive datatypes
+// It makes the shallow copy of the variable
+// There are 7 types of primitive datatypes
+// N => Number
+// N => null
+// B => Boolean
+// B => BigInt
+// S => String
+// S => Symbol
+// U => undefined
+
+// 2. Non-primitive datatypes
+// It makes the deep copy of the variable
+// There are 2 types of non-primitive datatypes
+// 1. Object
+// 2. Array
