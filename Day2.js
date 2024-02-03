2dd280e9b84386504611c98157b60ef35af8b34f
rohit-shinde-26 <100394511+rohit-shinde-26@users.noreply.github.com>
2024-02-03 14:36:06 +0000

    file has update

--- Day2.js
+++ Day2.js
@@ -1,4 +1,17 @@
-// Operators are shown for day 2
-console.log(
-  "****************************************Operator****************************************",
-);
+// sees some declaration using var
+var myName = "Rohit";
+var myName = "RAM";
+var myNam = "Rudra";
+console.log(myName);
+
+// sees with the let also
+let myName1 = "Rohit";
+// let myName1 = "RAM";
+myNam1 = "Rudra";
+console.log(myName1);
+
+// sees with const also
+const myName2 = "Rohit";
+// const myName2 = "RAM";
+// myNam2 = "Rudra";
+console.log(myName2);
