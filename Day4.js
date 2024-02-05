4c4873638d25d24711e6daab56322dbd98b6d40b
rohit-shinde-26 <100394511+rohit-shinde-26@users.noreply.github.com>
2024-02-05 03:21:19 +0000

    here the day 4 updated

---
+++ Day4.js
@@ -0,0 +1,31 @@
+// datatypes
+// primitive data types
+
+// string
+let str1 = "Hello";
+let str2 = "44";
+
+// console.log(str1);
+
+// console.log(str2);
+// console.log(typeof +str2);
+let newStr = Number(str2);
+// console.log(typeof newStr);
+
+// Number
+let num1 = 44;
+// console.log(typeof num1);
+// num1 = num1 + "";
+let myNum = String(num1);
+console.log(typeof num1);
+
+// Boolean
+// undefined
+let a;
+console.log(a);
+console.log(typeof a);
+
+// null
+let b = null;
+console.log(b);
+console.log(typeof b); // bug error
