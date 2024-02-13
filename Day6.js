8836b0cf043d6496c49348c8c33a0d403838d71f
rohit-shinde-26 <100394511+rohit-shinde-26@users.noreply.github.com>
2024-02-13 03:23:21 +0000

    da6 has ready

--- Day6.js
+++ Day6.js
@@ -1,1 +1,25 @@
 // after few days now we continue the practise on js
+let temp = 30;
+if (temp > 50) {
+  console.log("its hot");
+} else {
+  if (temp < 10) {
+    console.log("its cold");
+  } else {
+    console.log("its moderate");
+  }
+}
+
+// cases
+let day = +prompt("Enter the day");
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("thuesday");
+    break;
+
+  default:
+    console.log("Invalid day");
+}
