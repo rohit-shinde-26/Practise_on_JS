d88615067a32aff1b8c8aa99a546c28cff41b77a
rohit-shinde-26 <100394511+rohit-shinde-26@users.noreply.github.com>
2024-02-13 03:54:06 +0000

    the loops are done for the day

---
+++ Day7.js
@@ -0,0 +1,25 @@
+// while loop
+// we see the all loops in that file at once time
+// let i = 0;
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+// let i = 0;
+// let total = 0;
+// while (i <= 10) {
+//   total = total + i;
+//   i++;
+// }
+// console.log(total);
+
+// // for loop
+// for (let i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+let total = 0;
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+  total = total + i;
+}
+console.log(`Here is the total of all number ${total}`);
