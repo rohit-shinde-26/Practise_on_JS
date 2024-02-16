daa4f29ea05918f4471a9980b16742f54bcbbf01
rohit-shinde-26 <100394511+rohit-shinde-26@users.noreply.github.com>
2024-02-16 13:32:49 +0000

    loops covered is in day8

---
+++ Day8.js
@@ -0,0 +1,33 @@
+// let a = 0;
+// let i = 0;
+
+// for (i = 0; i < 10; i++) {
+//   a = a + i;
+// }
+// console.log(a);
+
+// break continue
+// let i = 0;
+
+// for (i = 0; i < 10; i++) {
+//   if (i == 5) {
+//     break;
+//   }
+//   console.log(i);
+// }
+
+// for (i = 0; i < 10; i++) {
+//   if (i == 5) {
+//     console.log("hello world");
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// do while
+let score = 0;
+do {
+  console.log(score);
+  score++;
+} while (score < 10);
+console.log("score is =>", score);
