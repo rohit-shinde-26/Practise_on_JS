7ed2bf46f64483293217ea45bf7a46757a345205
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-02-24 14:28:44 +0000

    done for the day16

---
+++ Day16.js
@@ -0,0 +1,20 @@
+// function isEven(number) {
+//   return number % 2 == 0;
+// }
+
+// console.log(isEven(4));
+
+// function isEven(number) {
+//   console.log(number % 2 == 0);
+// }
+
+// isEven(4);
+
+// function addNum(num1, num2) {
+//   return num1 + num2;
+// }
+// console.log(addNum(4, 2));
+function myStr(str) {
+  return str[2];
+}
+console.log(myStr("hello"));
