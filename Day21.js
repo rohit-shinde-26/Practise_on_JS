10c49242d7d0a1aecfde7b1554314f093a0fbde0
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-03-23 02:47:53 +0000

    object start today

---
+++ Day21.js
@@ -0,0 +1,38 @@
+// const friends = ["Ram", "Abhi", "Akshay"];
+// friends.push("Akay");
+
+// console.log(friends);
+
+// const [...myFriends] = friends;
+// console.log(myFriends);
+
+// console.log(friends === myFriends);
+// let i = 0;
+// while (i < friends.length) {
+//   console.log(friends[i]);
+//   i++;
+// }
+
+// for (let i = 0; i < friends.length; i++) {
+//   console.log(friends[i].toUpperCase());
+// }
+// for (let friend of friends) {
+//   console.log(friend);
+// }
+// for (let friend in friends) {
+//   console.log(`${friends[friend]}`);
+// }
+
+// const friends = ["Ram", "Abhi", "Akshay"];
+// const [id1, ...other] = friends;
+// console.log(id1)
+
+//objects
+// const showRoom = {
+//   name: "Mustang Gt",
+//   colour: "Red",
+//   price: 8000000,
+// };
+// for (let data in showRoom) {
+//   console.log(`${data} : ${showRoom[data]}`);
+// }
