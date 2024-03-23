0328d0c0209be00a52bc542feb1f8de4fd45e567
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-03-23 03:37:08 +0000

    rest hard parameter

--- Day21.js
+++ Day21.js
@@ -45,18 +45,89 @@
 // myObj
 // console.log(myObj);
 
-const carsData = {
-  car1: "Mustang",
-  car2: "Civic",
-  car3: "Verna Turbo",
-  car4: "City",
-};
-const { car1, ...other } = carsData;
-console.log(other);
+// const carsData = {
+//   car1: "Mustang",
+//   car2: "Civic",
+//   car3: "Verna Turbo",
+//   car4: "City",
+// };
+// const { car1, ...other } = carsData;
+// console.log(other);
 
-const showRoom = [
-  { id: 1, name: "Mustang", color: "Red" },
-  { id: 2, name: "Civic", color: "Voilet" },
-  { id: 3, name: "Supra", color: "White" },
-  { id: 4, name: "Verna", color: "Black" },
-];
+// const showRoom = [
+//   { id: 1, name: "Mustang", color: "Red" },
+//   { id: 2, name: "Civic", color: "Voilet" },
+//   { id: 3, name: "Supra", color: "White" },
+//   { id: 4, name: "Verna", color: "Black" },
+// ];
+
+// functions
+// function add(no1, no2) {
+//   console.log(no1 + no2);
+// }
+// add(3, 7);
+
+// const sub = function (num1, num2) {
+//   console.log(num1 - num2);
+// };
+// sub(8, 2);
+
+// const findStr = (str) => str[0];
+// console.log(findStr("Ram"));
+
+// console.log(add(9, 4));
+// function add(n1, n2) {
+//   return n1 + n2;
+// }
+
+// Default parameter
+// const add = (a, b) => {
+//   if (typeof b == "undefined") {
+//     b = 1;
+//   }
+//   return a + b;
+// };
+// console.log(add(4));
+// const add = (a, b = 4) => {
+//   return a + b;
+// };
+// console.log(add(4));
+
+// parameter destructuring
+const myObj = {
+  name: "ram",
+  age: 21,
+};
+
+// const myFunc = (obj) => {
+//   console.log(obj.name);
+// };
+
+// myFunc(myObj);
+// const myFunc = ({ name, age }) => {
+//   console.log(name);
+// };
+
+// myFunc(myObj);
+
+// rest
+function hello(...numbers) {
+  let total = 0;
+  for (let number of numbers) {
+    total = total + number;
+  }
+  return total;
+}
+const ans = hello(3, 4, 5, 6, 7, 8, 9);
+console.log(ans);
+
+// function addAll(...numbers){
+//   let total = 0;
+//   for(let number of numbers){
+//       total = total + number;
+//   }
+//   return total;
+// }
+
+// const ans = addAll(4,5,4,2,10);
+// console.log(ans);
