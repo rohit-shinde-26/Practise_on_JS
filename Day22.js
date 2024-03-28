1c64dfc61d1b09224ccf32bf05920793ad379226
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-03-28 02:46:41 +0000

    day22

---
+++ Day22.js
@@ -0,0 +1,24 @@
+// imp array methods
+
+// const myArr = ["Ram", "Sham", "Hari", "Om"];
+// const ans = myArr.find((str) => {
+//   return (str.length = 3);
+// });
+// console.log(ans);
+
+const nums = [2, 6, 4, 8, 2, 8];
+// const ans = nums.every((no) => {
+//   return no % 2 == 0;
+// });
+// console.log(ans);
+
+// const ans = nums.some((no) => {
+//   return no % 2 == 0;
+// });
+// console.log(ans);
+
+// const ans = nums.fill(0, 2, 4);
+// console.log(ans);
+
+const ans = nums.splice(0, 2, 1);
+console.log(ans);
